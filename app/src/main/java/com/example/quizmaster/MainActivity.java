ac19e5d0678e018fa82180f7241ffae2f5ab9c8d
Renzo Moran <rmoranortega9968@conestogac.on.ca>
2021-04-04 00:46:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.quizmaster;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.quizmaster.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Preparation for setting the Navigation Drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle (this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        SetNavigationDrawer();
+    }
+
+    private void SetNavigationDrawer() {
+        // Create the widget reference for the navigation view
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view); // id of the navigation menu in activity_main.xml
+        navView.setItemIconTintList(null);  // Important to show color icons rather than their gray shadows
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) { // method called each time a menu item is selected
+
+                // Define fragment depending on which menu option was selected
+                Fragment frag = null;
+                int itemId = item.getItemId();  // id of the item
+                if (itemId == R.id.history_quiz) {
+                    frag = new HistoryQuizFragment();
+                }
+                else if (itemId == R.id.math_quiz) {
+                    frag = new MathQuizFragment();
+                }
+                else if (itemId == R.id.settings) {
+                    frag = new SettingsFragment();
+                }
+                else if (itemId == R.id.statistics) {
+                    frag = new StatisticsFragment();
+                }
+
+                if (frag != null) {
+                    FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+                    trans.replace(R.id.frame, frag);
+                    trans.commit();
+                    mDrawerLayout.closeDrawers(); // close the menu
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;  // if an item is selected, return true
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Procedure to build message string
+    public void showMessage(String title, String messageContent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(messageContent);
+        builder.show();
+    }
+}
---
+++ app/src/main/java/com/example/quizmaster/MathQuizFragment.java
@@ -0,0 +1,71 @@
+package com.example.quizmaster;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quizmaster.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MathQuizFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MathQuizFragment extends Fragment {
+
+    View view;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MathQuizFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MathQuizFragment newInstance(String param1, String param2) {
+        MathQuizFragment fragment = new MathQuizFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
+    }
+
+}

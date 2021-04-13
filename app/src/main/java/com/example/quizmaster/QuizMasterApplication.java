620db26a6bb6dfb0294bd7fd270d0465b57e5acc
Jiyoug Jung <jjy5242@gmail.com>
2021-04-13 16:32:26 -0400

    change table name and columns

--- app/src/main/java/com/example/quizmaster/QuizMasterApplication.java
+++ app/src/main/java/com/example/quizmaster/QuizMasterApplication.java
@@ -24,14 +24,14 @@ public class QuizMasterApplication extends Application {
                     quiz_type: TF(True / False), MC(Multi Choice), WORDS(write words)
                 */
 
-                //toast.show();
-                db.execSQL("CREATE TABLE IF NOT EXISTS tbl_quiz(" +
-                        "quz_no INTEGER, quiz_category TEXT, quie_type TEXT, quiz_text TEXT, option1 TEXT, " +
+                toast.show();
+                db.execSQL("CREATE TABLE IF NOT EXISTS tbl_quiz_questions(" +
+                        "quest_no INTEGER, quest_category TEXT, quest_type TEXT, quest_text TEXT, option1 TEXT, " +
                         "option2 TEXT, option3 TEXT, option4 TEXT, answer TEXT )");
 
-                db.execSQL("CREATE TABLE IF NOT EXISTS tbl_result(" +
-                        "quiz_category TEXT, correct_answer INTEGER, wrong_answer INTEGER, " +
-                        "skip_answer INTEGER, answer_time REAL, score REAL, answer_current_date )");
+                db.execSQL("CREATE TABLE IF NOT EXISTS tbl_quiz_result(" +
+                        "quiz_category TEXT, correct_answer INTEGER, elapsed_time REAL, score REAL, time_stamp )");
+
 
                 //insertQuiz();
 
@@ -47,64 +47,64 @@ public class QuizMasterApplication extends Application {
     }
 
     public void insertQuiz(){
-        //Toast.makeText(this, "Insert Quiz", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Insert Quiz", Toast.LENGTH_LONG).show();
         SQLiteDatabase db =  helper.getReadableDatabase();
         Cursor cursor = db.rawQuery(
                 "SELECT COUNT(*) AS COUNT " +
-                        "FROM tbl_quiz", null);
+                        "FROM tbl_quiz_questions", null);
 
         cursor.moveToFirst();
         if(cursor.getInt(0) == 0){
             db = helper.getWritableDatabase();
 
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "1,'history','MC','Which country gifted the United States the Statue of Liberty?','England','Spain','France', 'Italy','France')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "2,'history','MC','This was the president of the Confederate states during the U.S. Civil War.','George Washington', 'Jefferson Davis', 'Abraham Lincoln','John Adams','Jefferson Davis')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "3,'history','TF','True or False? The Civil War ended in 1861.','True','False','', '', 'False')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "4,'history','MC','Which city was attacked by the first atomic bomb?','Nagasaki','Paris','Hiroshima', 'Mexico City', 'Hirosima')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "5,'history','MC','Who first discovered America?','Leif Eriksson','Christopher Columbus','Pierre de Monts', 'Amerigo Vespucci', 'Leif Eriksson')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "6,'history','MC','Who was the architect who rebuilt London after the Great Fire of 1666?','I.M. Pei','Sir Christopher Wren','Christopher Columbus', 'Sir Christopher Robin', 'Sir Christopher Wren')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "7,'history','MC','Which of these peoples once ruled Norway?','Vikings','Aztecs','Irish', 'Romans', 'Vikings')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "8,'history','MC','Which of these nations was neutral in World War I?','England','Germany','Italy', 'Norway', 'Norway')");
 
 
 
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "1,'math','WORDS','1 hundred, 6 tens and 2 ones?','','','', '', '162')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "2,'math','WORDS','A factory makes 9.8 metres of masking tape every minute. How many metres of masking tape can the factory make in 6 minutes?','','','', '', '58.8')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "3,'math','WORDS','A factory makes 9.8 metres of masking tape every minute. How many metres of masking tape can the factory make in 6 minutes?','','','', '', '58.8')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "4,'math','WORDS','Each cement block weighs 0.7 kilograms. How much do 4 blocks weigh in total?','','','', '', '2.8')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "5,'math','WORDS','Multiply: 14×0.3=','','','', '', '4.2')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "6,'math','WORDS','Multiply: 8×54=','','','', '', '432')");
-            db.execSQL("INSERT INTO tbl_quiz VALUES (" +
+            db.execSQL("INSERT INTO tbl_quiz_questions VALUES (" +
                     "7,'math','WORDS','Multiply: 692/100=','','','', '', '6.92')");
 
         }
 
-
+        cursor.close();
 
 
     }
-    public void addResult(String category, int correct, int wrong, int skip, float time){
+    public void addResult(String category, int correct, float time, float score){
         SQLiteDatabase db =  helper.getWritableDatabase();
 
         //1: Tie, 2: Lose, 3:Win
 
         db.execSQL("INSERT INTO tbl_result VALUES (" +
-                category + "," + correct + "," + wrong + "," + skip + "," + time + "," + correct * 100 / time +
-                ", " + Math.round(System.currentTimeMillis() / 1000)+")");
+                category + "," + correct + "," + time + "," + score + "," + correct * 100 / time +
+                ", " + Math.round(System.currentTimeMillis() / 1000.0)+")");
 
 
     }

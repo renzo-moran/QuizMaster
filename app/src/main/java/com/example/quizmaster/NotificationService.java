2e99f6beb4d7cd98819bb27f5b64c557849a4c10
Jiyoug Jung <jjy5242@gmail.com>
2021-04-23 09:57:12 -0400

    Add channel to higher version of API

--- app/src/main/java/com/example/quizmaster/NotificationService.java
+++ app/src/main/java/com/example/quizmaster/NotificationService.java
@@ -1,13 +1,19 @@
 package com.example.quizmaster;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.IBinder;
+import android.provider.SyncStateContract;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -17,12 +23,21 @@ import java.util.TimerTask;
 public class NotificationService extends Service {
     public NotificationService() {
     }
+    // This is the Notification Channel ID. More about this in the next section
+    public static final String NOTIFICATION_CHANNEL_ID="channel_id";
+
+    //User visible Channel Name
+    public static final String CHANNEL_NAME="Notification Channel";
+
+    // Importance applicable to all the notifications in this Channel
+    int importance=NotificationManager.IMPORTANCE_DEFAULT;
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onCreate() {
 
@@ -34,7 +49,20 @@ public class NotificationService extends Service {
         final PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent,
                                         PendingIntent.FLAG_UPDATE_CURRENT);
 
-        final Notification notification = new Notification.Builder(getApplicationContext())
+        // create channel in new versions of android
+        //if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= 26) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, CHANNEL_NAME, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            manager.createNotificationChannel(notificationChannel);
+        }
+
+
+        final Notification notification = new Notification.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID)
                                         .setSmallIcon(R.drawable.ic_notification_quiz)
                                         .setContentTitle(getString(R.string.app_name))
                                         .setContentText(getString(R.string.notifiation_message))
@@ -46,7 +74,7 @@ public class NotificationService extends Service {
             @Override
             public void run() {
 
-                toast.show();
+                //toast.show();
                 manager.notify(1, notification);
                 timer.cancel();
                 stopSelf();
@@ -58,7 +86,7 @@ public class NotificationService extends Service {
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
         super.onDestroy();
     }
 }
